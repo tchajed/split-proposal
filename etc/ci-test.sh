5856277a781e7323c9909783c1921be426ecddf4
Tej Chajed <chajed@wisc.edu>
2026-02-07 10:07:15 -0600

    Fix CI test for new output

--- etc/ci-test.sh
+++ etc/ci-test.sh
@@ -5,9 +5,9 @@ set -eu
 # assumes main.pdf is already built (using Makefile locally, or with github-action-for-latex)
 
 out=$(go run . -file sample/main.pdf -outDir sample)
-echo "$out" | grep "project-description.pdf: 2-5" 1>/dev/null
-echo "$out" | grep "data-mgmt-plan.pdf: 7-7" 1>/dev/null
-echo "$out" | grep "Tej-Chajed-synergistic-activities.pdf: 9-end" 1>/dev/null
+echo "$out" | grep "project-description.pdf: p2.*4 pages" 1>/dev/null
+echo "$out" | grep "data-mgmt-plan.pdf: p7" 1>/dev/null
+echo "$out" | grep "Tej-Chajed-synergistic-activities.pdf: p9" 1>/dev/null
 
 if [ ! -e sample/submit-project-description.pdf ]; then
   echo "project description not generated" 1>&2
